5a6746bb4322696a4e82b7748d7df6b43a3f7896
Oyeyebi Ridwan Oyewale <sp09iner@gmail.com>
2022-06-20 15:45:43 +0100

    error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+
+	// sets the prefix and the flag
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// request the message
+	message, err := greetings.Hello("")
+
+	if err != "" {
+		log.Fatal(err)
+	}
+
 	message := greetings.Hello("Ridwan")
 	fmt.Println(message)
 }
